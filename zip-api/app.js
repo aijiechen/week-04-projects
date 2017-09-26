36d492fd09881abf98ab54b734101b197953ff09
Aijie Chen <achen002@citymail.cuny.edu>
2017-09-26 18:40:57 -0400

    solution

--- zip-api/app.js
+++ zip-api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const zipdb = require('./zipData');
+const zipdb = require('./zipData');// export zip database
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,12 +15,25 @@ app.get('/', (req, res) => {
 
 
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+  const records =zipdb.byZip[req.params.zipcode];
+  if(records === undefined) {
+  	res.sendStatus(404);
+  }else{
+  	res.json(records);
+  }
 });
 
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
+    const records =zipdb.byCity[req.params.cityname];
+  if(records === undefined) {
+  	res.sendStatus(404);
+  }else{
+  	res.json(records);
+  }
+  
+
 });
 
 
